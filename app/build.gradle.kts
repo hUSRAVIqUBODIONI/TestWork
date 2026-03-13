53cf7dd2aff64783f9dfae034af5a0decff8c889
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2026-03-13 16:35:14 +0300

    added firebase, navigation libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
+    implementation("com.google.firebase:firebase-auth")
+
+    implementation("androidx.navigation:navigation-compose:2.9.7")
 }
