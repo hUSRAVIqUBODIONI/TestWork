a617f1c3a4403c47a4621416c92b2145dc462c8a
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2026-03-15 21:51:52 +0300

    added simple screens

---
+++ app/src/main/java/com/example/testwork/screens/Profile/ProfileScreen.kt
@@ -0,0 +1,9 @@
+package com.example.testwork.screens.Profile
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProfileScreen(){
+    Text("Экран профиля")
+}
