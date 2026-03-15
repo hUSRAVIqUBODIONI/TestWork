d8c382d65e34ecb795cd5c03dc6d067b88859be5
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2026-03-15 21:52:09 +0300

    added simple navigation

--- app/src/main/java/com/example/testwork/MainActivity.kt
+++ app/src/main/java/com/example/testwork/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.testwork.ui.theme.TestWorkTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,30 +15,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TestWorkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+
+            NavHost(navController = navController, startDestination = NavScreens.AuthScreen.route){
+                composable(route = NavScreens.AuthScreen.route){
+                    NavScreens.AuthScreen
+                }
+                composable(route = NavScreens.ProfileScreen.route){
+                    NavScreens.ProfileScreen
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+sealed class NavScreens(val route: String){
+    object AuthScreen : NavScreens("auth")
+    object ProfileScreen : NavScreens("profile")
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestWorkTheme {
-        Greeting("Android")
-    }
-}
