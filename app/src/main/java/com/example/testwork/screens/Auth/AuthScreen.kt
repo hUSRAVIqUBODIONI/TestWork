d8d9be6be5a38df01e93c9a146c5855308275750
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2026-03-16 18:29:32 +0300

    added TextFields

--- app/src/main/java/com/example/testwork/screens/Auth/AuthScreen.kt
+++ app/src/main/java/com/example/testwork/screens/Auth/AuthScreen.kt
@@ -1,9 +1,63 @@
 package com.example.testwork.screens.Auth
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun AuthScreen(){
-    Text("Экран Входа")
+fun AuthScreen(
+    paddingValues: PaddingValues,
+    viewModel: AuthViewModel = hiltViewModel()
+){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(paddingValues),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            label = { Text("Email") }
+        )
+        Spacer(Modifier.height(10.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            label = { Text("Password") }
+        )
+        Spacer(Modifier.height(15.dp))
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center) {
+            OutlinedButton(onClick = {}) {
+                Text(" Login ")
+            }
+            Spacer(Modifier.width(5.dp))
+            OutlinedButton(onClick = {}) {
+                Text("Register")
+            }
+        }
+    }
 }
