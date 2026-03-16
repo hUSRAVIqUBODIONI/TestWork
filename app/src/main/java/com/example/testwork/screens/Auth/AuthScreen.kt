8c301e09a71435980a8a3b8ba3aded02077d6e79
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2026-03-16 20:32:38 +0300

    added auth

--- app/src/main/java/com/example/testwork/screens/Auth/AuthScreen.kt
+++ app/src/main/java/com/example/testwork/screens/Auth/AuthScreen.kt
@@ -1,11 +1,11 @@
 package com.example.testwork.screens.Auth
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,22 +15,35 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.testwork.NavScreens
 
 @Composable
 fun AuthScreen(
     paddingValues: PaddingValues,
+    navController: NavController,
     viewModel: AuthViewModel = hiltViewModel()
 ){
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+
+    LaunchedEffect(key1 = viewModel.toastMessage) {
+        viewModel.toastMessage.collect { message ->
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize().padding(paddingValues),
@@ -51,11 +64,19 @@ fun AuthScreen(
         Spacer(Modifier.height(15.dp))
         Row(modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center) {
-            OutlinedButton(onClick = {}) {
+            OutlinedButton(onClick = {viewModel.logIn(email,password){ success ->
+                if (success) navController.navigate(NavScreens.ProfileScreen.route)
+                password = ""
+            } }) {
                 Text(" Login ")
             }
             Spacer(Modifier.width(5.dp))
-            OutlinedButton(onClick = {}) {
+            OutlinedButton(onClick = {
+                viewModel.registerUser(email,password){ success ->
+                    if (success) navController.navigate(NavScreens.ProfileScreen.route)
+                    password = ""
+                }
+            }) {
                 Text("Register")
             }
         }
--- app/src/main/java/com/example/testwork/screens/Auth/AuthViewModel.kt
+++ app/src/main/java/com/example/testwork/screens/Auth/AuthViewModel.kt
@@ -1,15 +1,49 @@
 package com.example.testwork.screens.Auth
 
 import android.util.Log
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AuthViewModel @Inject constructor(val auth : FirebaseAuth): ViewModel(){
     init {
         Log.d("Test","InitViewModel")
+    }
+
+    private val _toastMessage = MutableSharedFlow<String>()
+    val toastMessage = _toastMessage
+
+    private val context = LocalContext
+
+    fun registerUser(email: String, password: String,onSuccess : (Boolean) -> Unit){
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                onSuccess(true)
+            } else {
+                viewModelScope.launch {
+                    _toastMessage.emit(task.exception?.message ?: "Registration failed")
+                }
+            }
+        }
+    }
+
+    fun logIn(email: String, password: String,onSuccess : (Boolean) -> Unit){
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task->
+            if (task.isSuccessful){
+                onSuccess(true)
+            }else{
+                viewModelScope.launch {
+                    _toastMessage.emit(task.exception?.message ?: "Login failed")
+                }
+            }
+        }
     }
 }
